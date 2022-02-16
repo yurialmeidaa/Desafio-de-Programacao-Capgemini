33eee210c8cb9df65b88525d1407654a18475bce
yurialmeidaa <yuri1193_@hotmail.com>
2022-02-16 20:36:04 -0300

    questao 02 resolvida

--- src/br/com/desafio/Questao02.java
+++ src/br/com/desafio/Questao02.java
@@ -1,4 +1,51 @@
 package br.com.desafio;
 
+import java.util.Scanner;
+
 public class Questao02 {
+
+   /* public static boolean senhaForte(String senha) {
+        if (senha.length() < 6) return false;
+
+        boolean achouNumero = false;
+        boolean achouMaiuscula = false;
+        boolean achouMinuscula = false;
+        boolean achouSimbolo = false;
+        for (char c : senha.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                achouNumero = true;
+            } else if (c >= 'A' && c <= 'Z') {
+                achouMaiuscula = true;
+            } else if (c >= 'a' && c <= 'z') {
+                achouMinuscula = true;
+            } else {
+                achouSimbolo = true;
+            }
+        }
+        return achouNumero && achouMaiuscula && achouMinuscula && achouSimbolo;
+    }*/
+
+    public static void senhaMinima(String senha){
+        int tamanhoSenha = 0;
+        if (senha.length() < 6) {
+            tamanhoSenha = 6 - senha.length();
+            System.out.println(tamanhoSenha);
+        }else{
+            System.out.println("Senha com o tamanho minimo");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String senha;
+
+        System.out.println("Digite a senha desejada");
+
+        senha = scan.nextLine();
+
+        senhaMinima(senha);
+
+
+    }
 }
